f19021e5ca0afce29c4a430211b393fec7125719
Hezby <mail@hezby.com>
2018-08-29 18:28:01 +0700

    add test for question_controller

--- backend/spec/controllers/api/v1/questions_controller_spec.rb
+++ backend/spec/controllers/api/v1/questions_controller_spec.rb
@@ -3,61 +3,114 @@ require 'rails_helper'
 describe Api::V1::QuestionsController, type: :controller do
   describe '#index' do
     it 'correct data' do
+      get :index, format: :json
+
+      expect(response).to have_http_status(:success)
     end
   end
 
   describe '#show' do
     it 'correct data' do
+      get :show, id: question.id, format: :json
+
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'correct json' do
+      get :show, id: question.id, format: :json
+
+      expect(response.body).to eq(question.to_json)
     end
 
     it 'id is invalid' do
+      expect{
+        get :show, format: :json
+      }.to raise_error(ActionController::RoutingError)
     end
   end
 
   describe '#new' do
     it 'not found' do
+      expect{
+        get :new, format: :json
+      }.to raise_error(ActionController::RoutingError)
     end
   end
 
   describe '#create' do
+    let(:build_param) { build :question }
     it 'valid data' do
+      post :create, question: build_param.to_json, format: :json
+
+      expect(Question.last.name).to eq(build_param.name)
     end
 
     it 'valid data render json message' do
+      post :create, question: build_param.to_json
+
+      expect(JSON.parse(response.body)).to eq({message: 'success'})
     end
 
-    it 'invalid data' do
-    end
+    it 'invalid data render json' do
+      build_param.name = nil
 
-    it 'invalid data not render json message' do
+      post :create, question: build_param.to_json, format: :json
+
+      expect(JSON.parse(response.body)).to eq({message: 'failed', errors: { name: "can't be blank" }})
     end
   end
 
   describe '#update' do
+    let(:question) { build :question }
+    let(:quote) { Faker::Matz.quote }
+
+    before(:each) do
+      question.save
+    end
+
     it 'valid data' do
+      put :update, id: question.id, question: { name: quote }, format: :json
+
+      question.reload
+      expect(question.name).to eq(quote)
     end
 
     it 'valid data render json message' do
+      put :update, id: question.id, question: { name: quote }, format: :json
+
+      expect(JSON.parse(response.body)).to eq({message: 'success'})
     end
 
-    it 'invalid data' do
-    end
+    it 'invalid data render json message' do
+      put :update, id: question.id, question: { name: nil }, format: :json
 
-    it 'invalid data not render json message' do
+      expect(JSON.parse(response.body)).to eq({message: 'failed', errors: { name: "can't be blank" }})
     end
   end
 
   describe '#destroy' do
     it 'valid id' do
+      delete :destroy, id: question.id, format: :json
+
+      expect(Question.find(id)).to be_nil
     end
 
     it 'valid id render json message' do
+      delete :destroy, id: question.id, format: :json
+
+      expect(JSON.parse{response.body}).to eq({status: 'success', id: question.id})
     end
 
     it 'invalid id' do
+      expect{
+        delete :destroy, format: :json
+      }.to raise_error(ActionController::RoutingError)
     end
 
     it 'invalid id not render json message' do
+      delete :destroy, id: '1234', format: :json
+
+      expect(JSON.parse{response.body}).to eq({})
     end
   end
 end
