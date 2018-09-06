6b222b7f9c87ddc17674684372124bf17da3f656
Joy Paas <kjcpaas@gmail.com>
2018-09-06 12:21:59 +0800

    Add questions in quiz form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -25,6 +25,24 @@ const App = () => (
   </Router>
 );
 
+const questions = [
+  {
+    id: 1,
+    question: "What is the capital of Malaysia?",
+    answer: "Kuala Lumpur",
+  },
+  {
+    id: 2,
+    question: "How many letters are there in the English alphabet?",
+    answer: "26",
+  },
+  {
+    id: 3,
+    question: "Who are snow white's friends?",
+    answer: "7 dwarves",
+  },
+]
+
 const Home = () => (
   <div>
     Welcome to Quiz master
@@ -34,8 +52,21 @@ const Home = () => (
 const Quiz = () => (
   <div>
     Here's your quiz!
+
+    <form>
+      { questions.map(question => <QuizQuestionItem question={question} />) }
+
+      <button type="submit">Submit</button>
+    </form>
   </div>
 );
+
+const QuizQuestionItem = ({question}) => (
+  <div>
+    <p>{question.question}</p>
+    <input type="text"/>
+  </div>
+)
 
 const Admin = () => (
   <div>
