78ff77b62a9343e0ac9af536671e99dcb06efe8c
Hezby <mail@hezby.com>
2018-09-10 15:36:11 +0700

    add TODO instruction

--- backend/app/controllers/api/v1/questions_controller.rb
+++ backend/app/controllers/api/v1/questions_controller.rb
@@ -1,2 +1,3 @@
 class Api::V1::QuestionsController < Api::V1::BaseController
+  # TODO: please see test file for what should you do
 end
--- backend/app/controllers/api/v1/quizzes_controller.rb
+++ backend/app/controllers/api/v1/quizzes_controller.rb
@@ -1,2 +1,3 @@
 class Api::V1::QuizzesController < Api::V1::BaseController
+  # TODO: please see test file for what should you do
 end
