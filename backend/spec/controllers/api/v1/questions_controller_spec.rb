41b51300ba52679e41e930023b98b9c87d71f422
Nurahmadie <nurahmadie@gmail.com>
2018-10-18 11:02:38 +0700

    Fix missing params for request tests
    - Also remove spec for #new as rails API doesn't generate #new anymore

--- backend/spec/controllers/api/v1/questions_controller_spec.rb
+++ backend/spec/controllers/api/v1/questions_controller_spec.rb
@@ -17,29 +17,21 @@ describe Api::V1::QuestionsController, type: :controller, level_one: true, level
 
   describe '#show' do
     it 'responds with a success HTTP status' do
-      get :show, id: question.id, format: :json
+      get :show, params: { id: question.id }, format: :json
 
       expect(response).to have_http_status(:success)
     end
 
     it 'returns a JSON with correct data' do
-      get :show, id: question.id, format: :json
+      get :show, params: { id: question.id }, format: :json
 
       expect(response.body).to eq(question.to_json)
     end
 
     it 'raises a routing error if id is invalid' do
       expect{
-        get :show, id: 123456787899, format: :json
-      }.to raise_error(ActionController::RoutingError)
-    end
-  end
-
-  describe '#new' do
-    it 'raises a routing error' do
-      expect{
-        get :new, format: :json
-      }.to raise_error(ActionController::RoutingError)
+        get :show, params: { id: 123456787899 }, format: :json
+      }.to raise_error(ActiveRecord::RecordNotFound)
     end
   end
 
@@ -47,14 +39,14 @@ describe Api::V1::QuestionsController, type: :controller, level_one: true, level
     let(:build_param) { build :question }
 
     it 'able to create data' do
-      post :create, question: build_param.to_json, format: :json
+      post :create, params: { question: build_param.to_json }, format: :json
 
       expect(Question.last.content).to eq(build_param.content)
     end
 
     context 'with valid data' do
       it 'returns success message in JSON format' do
-        post :create, question: build_param.to_json
+        post :create, params: { question: build_param.to_json }, format: :json
 
         expect(JSON.parse(response.body)).to eq({message: 'success'})
       end
@@ -64,7 +56,7 @@ describe Api::V1::QuestionsController, type: :controller, level_one: true, level
       it 'returns error message in JSON format' do
         build_param.content = nil
 
-        post :create, question: build_param.to_json, format: :json
+        post :create, params: { question: build_param.to_json }, format: :json
 
         expect(JSON.parse(response.body)).to eq({message: 'failed', errors: { content: "can't be blank" }})
       end
@@ -80,7 +72,7 @@ describe Api::V1::QuestionsController, type: :controller, level_one: true, level
     let(:quote) { Faker::Matz.quote }
 
     it 'able to update data' do
-      put :update, id: question.id, question: { content: quote }, format: :json
+      put :update, params: { id: question.id, question: { content: quote } }, format: :json
 
       question.reload
       expect(question.content).to eq(quote)
@@ -88,7 +80,7 @@ describe Api::V1::QuestionsController, type: :controller, level_one: true, level
 
     context 'with valid data' do
       it 'returns success message in JSON format' do
-        put :update, id: question.id, question: { content: quote }, format: :json
+        put :update, params: { id: question.id, question: { content: quote } }, format: :json
 
         expect(JSON.parse(response.body)).to eq({message: 'success'})
       end
@@ -96,7 +88,7 @@ describe Api::V1::QuestionsController, type: :controller, level_one: true, level
 
     context 'with invalid data' do
       it 'invalid data render json message' do
-        put :update, id: question.id, question: { content: nil }, format: :json
+        put :update, params: { id: question.id, question: { content: nil } }, format: :json
 
         expect(JSON.parse(response.body)).to eq({message: 'failed', errors: { content: "can't be blank" }})
       end
@@ -110,14 +102,14 @@ describe Api::V1::QuestionsController, type: :controller, level_one: true, level
 
   describe '#destroy' do
     it 'able to destroy data if id valid' do
-      delete :destroy, id: question.id, format: :json
+      delete :destroy, params: { id: question.id }, format: :json
 
       expect(Question.find(question.id)).to be_nil
     end
 
     context 'with valid id' do
       it 'returns success message and deleted id in JSON format' do
-        delete :destroy, id: question.id, format: :json
+        delete :destroy, params: { id: question.id }, format: :json
 
         expect(JSON.parse{response.body}).to eq({status: 'success', id: question.id})
       end
@@ -131,7 +123,7 @@ describe Api::V1::QuestionsController, type: :controller, level_one: true, level
       end
 
       it 'does not return any JSON' do
-        delete :destroy, id: '1234', format: :json
+        delete :destroy, params: { id: '1234' }, format: :json
 
         expect(JSON.parse{response.body}).to eq({})
       end
@@ -141,7 +133,7 @@ describe Api::V1::QuestionsController, type: :controller, level_one: true, level
   describe '#answer' do
     context 'given correct answer' do
       it 'returns JSON with success message' do
-        post :answer, id: question.id, question: { answer: question.answer }, format: :json
+        post :answer, params: { id: question.id, question: { answer: question.answer } }, format: :json
 
         expect(JSON.parse{response.body}).to eq({status: 'success'})
       end
@@ -149,7 +141,7 @@ describe Api::V1::QuestionsController, type: :controller, level_one: true, level
 
     context 'given incorrect answer' do
       it 'returns JSON with success message' do
-        post :answer, id: question.id, question: { answer: 'sdasd' }, format: :json
+        post :answer, params: { id: question.id, question: { answer: 'sdasd' } }, format: :json
 
         expect(JSON.parse{response.body}).to eq({status: 'success'})
       end
@@ -157,7 +149,7 @@ describe Api::V1::QuestionsController, type: :controller, level_one: true, level
 
     context 'given blank answer' do
       it 'render JSON with error message' do
-        post :answer, id: question.id, question: { answer: '' }, format: :json
+        post :answer, params: { id: question.id, question: { answer: '' } }, format: :json
 
         expect(JSON.parse(response.body)).to eq({message: 'failed', errors: { content: "can't be blank" }})
       end
