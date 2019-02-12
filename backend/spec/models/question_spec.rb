50530ce14dafec8518094ab14e70ec42b98968a2
Eugie Limpin <eugiellimpin@gmail.com>
2019-02-12 19:35:40 +0800

    Add more tests

--- backend/spec/models/question_spec.rb
+++ backend/spec/models/question_spec.rb
@@ -15,16 +15,30 @@ describe Question, type: :model do
   end
 
   describe "#check_answer" do
-    it "is false if the answer is not valid" do
+    it "returns false if the answer is incorrect" do
       expect(question.check_answer('100')).to be(false)
     end
 
-    it "is number to number to word insensitive" do
-      expect(question.check_answer('10')).to be(true)
+    context "answer is number" do
+      it "accepts answer as number" do
+        expect(question.check_answer('10')).to be(true)
+      end
+
+      it "accepts answer as number in words" do
+        expect(question.check_answer("TEN")).to be(true)
+      end
     end
 
-    it "is number to word to number insensitive" do
-      expect(question.check_answer("TEN")).to be(true)
+    context "answer is number in words" do
+      let(:another_question) { create(:question, content: "one + one is?", answer: "two") }
+
+      it "accepts answer as number" do
+        expect(another_question.check_answer('2')).to be(true)
+      end
+
+      it "accepts answer as number in words" do
+        expect(another_question.check_answer("TWO")).to be(true)
+      end
     end
 
     it "is case insensitive" do
